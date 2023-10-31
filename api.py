80f3e1649cc8ea7e6f77b78be06cd3bb9194ff83
Pedro Chame <pedrohik@gmail.com>
2023-10-31 16:19:29 -0300

    Atualização

--- api.py
+++ api.py
@@ -165,4 +165,4 @@ def deletarJogoPorId(id):
 
 # Executando programa
 # app.run(port=5000, host='localhost', debug=True)
-app.run(port=80, host='0.0.0.0', debug=False)
+# app.run(port=80, host='0.0.0.0', debug=False)
